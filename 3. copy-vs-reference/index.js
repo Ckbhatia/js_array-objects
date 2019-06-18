650b53635a6a5714a4bea35f820cabcc01d8d3d4
Chetan Kumar <chetansain86@gmail.com>
2019-06-18 12:50:15 +0530

    complete copy-vs-reference assignment

--- 3. copy-vs-reference/index.js
+++ 3. copy-vs-reference/index.js
@@ -2,39 +2,39 @@
 
 var x = 10;
 var y = "abc";
-var a = x;
-var b = y;
+var a = x; // 10
+var b = y; // "abc"
 
 // Value of x, y, a, b
 var x = 10;
 var y = "abc";
-var a = x;
-var b = y;
-a = 5;
-b = "def";
+var a = x; // 10
+var b = y; // "abc"
+a = 5; // 5
+b = "def"; // "def"
 
 // Value of arr & arrCopy
 var arr = [1];
-var arrCopy = arr;
-arr.push(2);
+var arrCopy = arr; // [1] from arr
+arr.push(2); // [1, 2]
 
 // Value of obj
 var obj = { name: "ryan" };
-obj = { surname: "florance" };
+obj = { surname: "florance" }; // overwritten object value
 
 // What's the output.
 var arr = ["Hi"];
-var arr2 = arr;
-console.log(arr === arr2);
+var arr2 = arr; // reference of arr copied to arr2
+console.log(arr === arr2); // true
 
 // What's the output.
 var arr1 = ["Hi!"];
 var arr2 = ["Hi!"];
-console.log(arr1 === arr2);
-console.log(arr == arr2);
+console.log(arr1 === arr2); // false
+console.log(arr == arr2); // false
 
 //What's the output
-console.log([10] === [10]);
+console.log([10] === [10]); // false
 
 // What's the output?
 
@@ -52,29 +52,29 @@ var personObj1 = {
   age: 30
 };
 var personObj2 = personDetails(personObj1);
-console.log(personObj1); // -> ?
-console.log(personObj2); // -> ?
+console.log(personObj1); // personObj1 name: "Alex", age: 30
+console.log(personObj2); // personObj2 name: "John", age: 50
 
 // Guess the output
 var oldArray = [];
 var object = {};
-object.newArray = oldArray;
-oldArray.push(10);
-console.log(object.newArray === oldArray);
+object.newArray = oldArray; // Assign an new newArray key with oldArray. An empty array
+oldArray.push(10); // Push 10 to empty oldArray
+console.log(object.newArray === oldArray); // true
 
 // Guess the output
 var a = 5;
 var b = a;
 a = 10;
-console.log(a);
-console.log(b);
+console.log(a); // 10
+console.log(b); // 5
 
 // What's the output?
 var a = {};
 var b = a;
 a.a = 1;
-console.log(a);
-console.log(b);
+console.log(a); // Object a with a: 1 key and value
+console.log(b); // Object b with a: 1 key and value
 
 // What's the output.
 var a = [];
@@ -82,7 +82,7 @@ var b = a;
 a.push(1);
 console.log(a); // [1]
 console.log(b); // [1]
-console.log(a === b);
+console.log(a === b); // true
 
 // Clone the object person in clone
 var person = {
@@ -94,4 +94,4 @@ var person = {
   }
 };
 
-var clone = {};
+var clone = Object.assign({}, person);
